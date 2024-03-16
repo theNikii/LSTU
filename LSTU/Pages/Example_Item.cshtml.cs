3a504c8d22c769624ee416aedd201acec96a0e5a
N1k <Nik.shaboldin@yandex.ru>
2024-03-16 15:17:53 +0300

    макет предметы

---
+++ LSTU/Pages/Example_Item.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LSTU.Pages
+{
+    public class Exanple_ItemModel : PageModel
+    {
+        private readonly ILogger<Exanple_ItemModel> _logger;
+
+        public Exanple_ItemModel(ILogger<Exanple_ItemModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
